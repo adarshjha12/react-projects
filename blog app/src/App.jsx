54f36423076e48b467e1b7dd1eb4acaf4aaefd83
adarsh <jhaa50872@gmail.com>
2024-12-03 22:37:31 +0530

    authentication servise made

---
+++ blog app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import config from './conf/conf'
+
+function App() {
+
+  console.log(config.appwrite_url);
+  
+  return (
+    <>
+      <h1 className='bg-black text-white'>zdjn</h1>
+    </>
+  )
+}
+
+export default App
---
+++ blog app/src/appwrite/auth_service.js
@@ -0,0 +1,58 @@
+import config from "../conf/conf";
+
+import {Client, Account, ID} from 'appwrite'
+
+class AuthService {
+    client = new Client()
+    account;
+    constructor() {
+        this.client.setEndpoint(config.appwrite_url).setProject(config.appwrite_project_id)
+
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.login({email, password})
+            } else{
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({email, password}){
+
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+             console.log(error);
+             
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            return this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+}
+
+const authService = new AuthService()
+export default authService
