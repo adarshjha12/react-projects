44517a966e176aa4efd3d6c223639ecb055726e2
adarsh <jhaa50872@gmail.com>
2024-12-09 19:38:39 +0530

    added submit method in post form component

--- blog app/src/components/post-form/PostForm.jsx
+++ blog app/src/components/post-form/PostForm.jsx
@@ -20,10 +20,45 @@ function PostForm(post) {
      const navigate = useNavigate()
      const userData = useSelector(state => state.auth.userdata)
 
-     
+     const submit = async (data) =>{
+        if (post) {
+          const file = data.image[0] ? fileService.fileUpload(data.image[0]): null
+
+          if (file) {
+            fileService.fileDelete(post.featuredImage)
+          }
+
+          const dbPost = await fileService.updatePost(
+            post.$id, {
+              ...data,
+              featuredImage: file ? file.$id : undefined
+            }
+          )
+
+          if (dbPost) {
+            navigate(`/post/${dbPost.$id}`)
+          }
+        } else{
+          const file = data
+          .image[0] ? fileService.fileUpload(data.image[0]): null
+
+          if (file) {
+            const fileId = file.$id
+            data.featuredImage = fileId
+            const dbPost = await fileService.createPost({
+              ...data,
+              userId: userData.$id
+            })
+
+            if (dbPost) {
+              navigate(`/post/${dbPost.$id}`)
+            }
+          }
+        }
+     }
 
      const slugTransform = () =>{
-
+      
      }
 
   return (
