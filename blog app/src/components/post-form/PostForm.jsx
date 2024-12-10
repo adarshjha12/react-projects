be4cce903afcf724c90b7ff571009f1c72fb77a1
adarsh <jhaa50872@gmail.com>
2024-12-10 22:19:30 +0530

    added some child components in form

--- blog app/src/components/post-form/PostForm.jsx
+++ blog app/src/components/post-form/PostForm.jsx
@@ -66,17 +66,77 @@ function PostForm(post) {
      }, [])
 
      useEffect(() => {
-      const subscription = watch(() => {})
-      
+      const subscription = watch((value, {name}) => {
+        if (name === 'title') {
+          setValue('slug', slugTransform(value.title, {shouldValidate:true}))
+        }
+      })
+
       return () => {
         subscription.unsubscribe()
       }
      }, [watch, slugTransform, setValue]);
 
   return (
-    <div>
-        
-    </div>
+    <form action="">
+      <div className='w-2/3 px-2'>
+          <InputField
+            label = 'Title:'
+            className = 'mb-4'
+            placeholder = 'title'
+            {...register('title', {required: true})}
+          />
+
+          <InputField
+            label = 'Slug:'
+            className = 'mb-4'
+            placeholder = 'slug'
+            {...register('slug', {required: true})}
+            onInput={(e) =>{
+                setValue('slug', slugTransform(e.currentTarget.value, {shouldValidate: true}))
+            }}
+          />
+
+          <RTE
+            label='Content:'
+            name='content'
+            control={control}
+            defaultValue={getValues('content')}
+          />
+      </div>
+
+      <div className='w-1/3 px-2'>
+          <InputField
+            label = 'Featured Image:'
+            type = 'file'
+            className = 'mb-4'
+            accept = 'image/png, image/jpg, image/jpeg, image/gif'
+            {...register('image', {required: !post})}
+          />
+
+          {post && (
+            <div className='w-full mb-4'>
+                <img src={fileService.getFilePreview(post.featuredImage)}
+                 alt={post.title} className='rounded-lg' />
+            </div>
+          )}
+
+          <SelectBox
+            options = {["active", "inactive"]}
+            label= "Status"
+            className= 'mb-4'
+            {...register('status', {required: true})}
+          />
+
+          <Button
+            type='submit'
+            bgcolor={post? bg-green-600 : undefined}
+            classname='w-full'
+          >
+          {post ? 'Update' : 'Submit'}
+          </Button>
+      </div>
+    </form>
   )
 }
 
