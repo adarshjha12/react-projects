9c8ca37fe939ccd04e0ba6ca094897cc9fd64939
adarsh <jhaa50872@gmail.com>
2024-12-15 14:28:11 +0530

    made addpost component

--- blog app/src/pages/AllPostPage.jsx
+++ blog app/src/pages/AllPostPage.jsx
@@ -1,9 +1,35 @@
 
 import React from 'react'
+import {Container, Card} from '../components/index'
+import { useState } from 'react'
+import fileService from '../appwrite/fileServices'
+
 
 function AllPostPage() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fileService.getPosts([])
+    .then((posts) => {
+      if (posts) {
+        setPosts(posts.documents)
+      }
+    })
+    .catch((err) => console.log(err)
+    )
+    
+  }, []);
   return (
-    <div>AllPostPage</div>
+    <div className='w-full px-7'>
+      <Container>
+        <div className='flex flex-wrap'>
+            {posts.map((post) => {
+              <div key={post.$id}>
+                <Card post={post}/>
+              </div>
+          })}
+        </div>   
+      </Container>
+    </div>
   )
 }
 
