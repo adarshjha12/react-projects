34b8da86ac4fd62867be76ae694b40f4e0f5276b
adarsh <jhaa50872@gmail.com>
2024-12-15 23:53:14 +0530

    made post page

--- blog app/src/pages/Post.jsx
+++ blog app/src/pages/Post.jsx
@@ -1,1 +1,60 @@
-rfce
+import React, {useEffect, useState} from 'react'
+import {Button, Container} from '../components/index'
+import fileService from '../appwrite/fileServices';
+import { Link, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import parse from 'html-react-parser'
+import { useSelector } from 'react-redux';
+
+function Post() {
+    const [post, setPost] = useState([])
+    const {slug} = useParams()
+    const navigate = useNavigate()
+    const userData = useSelector(state => state.auth.userData)
+
+    const isAuthor = post && userData ? userData.$id === post.userId : false
+
+    useEffect(() => {
+        if (slug) {
+            
+            fileService.getPost(slug)
+            .then((post)=>{
+                if (post) setPost(post)
+                    else navigate('/')
+            })
+        } else{
+            navigate('/')
+        }
+    }, [slug, navigate]);
+
+
+    const deletePost = () =>{
+        fileService.deletePost(post.$id)
+        .then((status)=>{
+            if (status) {
+                fileService.fileDelete(post.featuredImage)
+                navigate('/')
+            }
+        })
+    }
+
+  return post ? (
+    <div>
+        <Container>
+            <div className='w-full flex justify-center mb-4 relative'>
+                <img src={fileService.getFilePreview(post.featuredImage)} alt={post.title} />
+
+                {isAuthor && (
+                    <div>
+                        <Link to={`/`}>
+
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </Container>
+    </div>
+  ) : ()
+}
+
+export default Post
