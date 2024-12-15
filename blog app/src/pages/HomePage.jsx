ef7eed8c5cdd664b566784dc040f56db340f7be0
adarsh <jhaa50872@gmail.com>
2024-12-15 23:08:12 +0530

    made homepage

---
+++ blog app/src/pages/HomePage.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import {Container, Card} from '../components/index'
+import fileService from '../appwrite/fileServices'
+
+
+function HomePage() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fileService.getPosts()
+        .then((posts) => {
+            if (posts) {
+                setPosts(posts)
+            }
+        })
+        .catch((err) => console.log(err)
+        )
+        
+    }, []);
+  
+    if (posts.length === 0) {
+        return (
+            <div>
+                <h1>you don't have any posts</h1>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Container>
+                {posts.map((post) => {
+                    <div key={post.$id}>
+                        <Card post={post}/>
+                    </div>
+                })}
+            </Container>
+        </div>
+    )
+}
+
+export default HomePage
