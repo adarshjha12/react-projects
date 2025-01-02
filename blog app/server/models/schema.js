c79df5ed84f602ce225a3f4c92062beef6fe900a
adarsh <jhaa50872@gmail.com>
2025-01-02 23:15:05 +0530

    account schema configured

---
+++ blog app/server/models/schema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (value) {
+                return validator.isEmail(value)
+            },
+            message: (props) => `$(props.value) is not valid email`
+        }
+    },
+    password:{
+        type: String,
+        required: true
+    },
+})
