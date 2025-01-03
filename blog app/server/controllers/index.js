7337e34da01ae66852d8fcfc014f4166ddb31bad
adarsh <jhaa50872@gmail.com>
2025-01-03 23:25:38 +0530

    small changes

---
+++ blog app/server/controllers/index.js
@@ -0,0 +1,5 @@
+const User = require('../models/schema')
+
+const createUser = new User({
+    title: req
+})
--- blog app/server/index.js
+++ blog app/server/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const config = require('./config/config')
 require('./models/connection')
 const port = config.server.port
-
+require('./controllers/index')
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send('welcome adarsh')
