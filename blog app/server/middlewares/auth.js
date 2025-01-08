7074caee93f2764ccec5f4e02d89ba0236755220
adarsh <jhaa50872@gmail.com>
2025-01-08 15:16:39 +0530

    auth middleware added

---
+++ blog app/server/middlewares/auth.js
@@ -0,0 +1,18 @@
+const config = require('../config/config')
+const jwtSecret = config.jwt.secret
+const jwt = require('jsonwebtoken')
+
+const verifyToken = async function (req, res, next) {
+        const token = req.headers['authorization']?.split(' ')[1]
+        if(!token) return res.status(403).send({message: 'no tokens found'})
+    
+            try {
+                const decodedUser = await jwt.verify(token, jwtSecret)
+                req.user = decodedUser
+                next()
+            } catch (error) {
+                res.status(401).json({message: 'invalid token'})
+            }
+}
+
+module.exports = verifyToken
