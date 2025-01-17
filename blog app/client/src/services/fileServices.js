ed8717af3f6c976b94acbec9a5ae98d8868c2fd6
adarsh <jhaa50872@gmail.com>
2025-01-18 00:41:49 +0530

    fix: update createPost and add updatePost functions with error handling

--- blog app/client/src/services/fileServices.js
+++ blog app/client/src/services/fileServices.js
@@ -2,10 +2,22 @@ import axios from "axios"
 
 const createPost = async function ({title, content, slug, status, featuredImage}) {
     try {
-        const response = axios.post('http://localhost:3000/user/login', {title, content, slug, status, featuredImage})
-        return (await response).data
+        const response = await axios.post('http://localhost:3000/user/login', {title, content, slug, status, featuredImage})
+
+        return response.data
     } catch (error) {
         console.log(error);
         
     }
 }
+
+const updatePost = async function (slug, {title, content, status, featuredImage}) {
+    try {
+        const response = await axios.post('http://localhost:3000/user/login',slug, {title, content, status, featuredImage})
+        return response.data
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
